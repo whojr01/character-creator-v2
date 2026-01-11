92ea54d1f9e90ad8face32bf2c73e0bf75c0e987
Bill Oliver <boliver@whojr.com>
2026-01-10 22:19:55 -0500

    Excercises 1 -5 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      {/* Excercise 4 */}
+      <div className="bar"></div>
       <CharacterEditor />
       <Footer />
     </>
